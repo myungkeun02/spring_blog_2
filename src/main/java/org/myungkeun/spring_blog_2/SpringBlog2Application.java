1946677cb7dc68d31bfd30db19f941aa8e9e6bc4
myungkeun <audrms3568@gmail.com>
2024-03-28 20:21:18 +0900

    connect db

---
+++ src/main/java/org/myungkeun/spring_blog_2/SpringBlog2Application.java
@@ -0,0 +1,13 @@
+package org.myungkeun.spring_blog_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlog2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlog2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/myungkeun/spring_blog_2/SpringBlog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myungkeun.spring_blog_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlog2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
